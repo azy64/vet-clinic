bcf90e32907c5c8ff0b22b6f2225ed63ead3de8a
azy64 <allysaidi64@gmail.com>
2021-12-06 21:48:46 +0100

    Add code to insert into animals

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id int,
+    id int GENERATED ALWAYS AS IDENTITY,
     name varchar(100),
     date_of_birth date,
     escape_attempts int,
     neutered boolean, 
     weight_kg real,
+    primary key(id)
 );
